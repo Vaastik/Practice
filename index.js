11aabf5a1020822bff37c243c181a53993218057
vaastik <matra.upadhyay@gmail.com>
2023-04-19 18:53:30 +0530

    code updated (querySelector operation)

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var item = document.getElementsByClassName("list-group-item");
+// var item = document.getElementsByClassName("list-group-item");
 // console.log(item);
-console.log(item[1]);
-item[1].style.backgroundColor='green';
-for(var i=0; i<item.length; i++)
-{
-    item[i].style.fontWeight='bold';
-}
+// console.log(item[1]);
+// item[1].style.backgroundColor='green';
+// for(var i=0; i<item.length; i++)
+// {
+//     item[i].style.fontWeight='bold';
+// }
+
+var list = document.querySelectorAll(".list-group-item");
+
+list[1].style.backgroundColor='green';
+list[2].style.display='none';
